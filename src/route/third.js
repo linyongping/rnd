94f3eec0dc852e3275bebc18ccfce2079c9a98f3
Sean <linyongping@outlook.com>
2017-11-10 00:19:59 +0800

    update dependencies

---
+++ src/route/third.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+class Third extends Component {
+    state = {  }
+    render() {
+        return (
+            <View>
+                <Text> this is Third Page </Text>
+            </View>
+        );
+    }
+}
+
+export default Third
